69b773f5c423c5bc2e79f061848569d17057331c
wathika-eng <wathika02@gmail.com>
2025-08-22 19:09:41 +0300

    feat: implement binary search function

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,25 @@
+// list must be sorted before
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    // lowest index
+    let low = 0;
+    // last element
+    let high = haystack.length - 1;
+    // keep searching till end
+    while (low <= high) {
+        // get the midpoint as an int
+        let mid = Math.floor(low + (high - low) / 2);
+        // index at mid point
+        let v = haystack[mid];
+        // if mid == index we are looking for, return early
+        if (needle === v) {
+            return true;
+        } else if (v < needle) {
+            // if value at mid is less than needle, we shift and scan the half right
+            low = mid + 1;
+        } else {
+            // else scan half left, shift end
+            high = mid - 1;
+        }
+    }
+    return false;
 }
